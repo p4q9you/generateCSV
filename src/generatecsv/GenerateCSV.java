30d3a48889f2491314867abb6139c1d000bc82da
heroku99una <m8oakl@gmail.com>
2018-07-29 22:04:01 +0900

    cording finish

--- src/generatecsv/GenerateCSV.java
+++ src/generatecsv/GenerateCSV.java
@@ -1,11 +1,11 @@
 package generatecsv;
 
-import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.DecimalFormat;
 
 /**
  *
@@ -19,31 +19,32 @@ public class GenerateCSV {
     public static void main(String[] args) {
 
         int fileOfNumber = 10;
+        int initialValue = 50;
 
-        // initial value
-        String csvInitValue1st = "001";
-        String csvInitValue2nd = "0000000001";
-        String csvInitValue3rd = "00000001";
+        // variable value
+        DecimalFormat digits_3 = new DecimalFormat("000");
+        DecimalFormat digits_8 = new DecimalFormat("00000000");
+        DecimalFormat digits_10 = new DecimalFormat("0000000000");
 
-        // content value
-        String[] fixedValueList = {"001",
-            "CSV", "0"};
-
-        String[] variableValueList = {csvInitValue1st,
-            csvInitValue2nd,
-            csvInitValue3rd};
+        // fixed value
+        String[] fixedValueList = {"001", "CSV", "0"};
 
         for (int counter = 0; counter < fileOfNumber; counter++) {
 
+            String[] variableValueList = {digits_3.format(initialValue),
+                digits_8.format(initialValue),
+                digits_10.format(initialValue)};
+
             // filename
             String prefix = "001_";
-            String contents = "00000001";
-            contents = contents + counter;
+            String contents = digits_8.format(initialValue);
             String extention = ".csv";
             String filename = prefix + contents + extention;
 
             // generate csv
             exportCsv(variableValueList, fixedValueList, filename);
+
+            initialValue++;
         }
     }
 
@@ -62,10 +63,18 @@ public class GenerateCSV {
             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileFullPath), "Shift-Jis")));
 
             // 内容をセットする
-            for (int counter = 0; counter < fixedValueList.length; counter++) {
-                printWriter.print(fixedValueList[counter]);
-                printWriter.print(",");
-            }
+            printWriter.print(fixedValueList[0]);
+            printWriter.print(",");
+            printWriter.print(fixedValueList[1]);
+            printWriter.print(",");
+            printWriter.print(fixedValueList[2]);
+            printWriter.print(",");
+
+            printWriter.print(variableValueList[0]);
+            printWriter.print(",");
+            printWriter.print(variableValueList[1]);
+            printWriter.print(",");
+            printWriter.print(variableValueList[2]);
 
             // ファイルに書き出し閉じる
             printWriter.close();
